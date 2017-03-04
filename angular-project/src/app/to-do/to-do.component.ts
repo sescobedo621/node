1ca7ecd5c75c78e5b275869e6ab9c3b1d2796d3e
Shelby Escobedo <shelbyescobedo@Shelbys-MacBook-Pro.local>
2017-03-04 18:14:28 -0500

    can add a task

--- angular-project/src/app/to-do/to-do.component.ts
+++ angular-project/src/app/to-do/to-do.component.ts
@@ -7,8 +7,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ToDoComponent implements OnInit {
   taskTitle = "Tasks";
-  tasks =["Learn Angular 2", "Take a Shower", "take metro to see Brenton"];
+  tasks =["Learn Angular 2", "Take a Shower", "work", "blah", "blah"];
+  to = "add";
 
+  addTasks(task){
+    this.to = task;
+    this.tasks.push(this.to);
+  }
   constructor() { }
 
   ngOnInit() {
